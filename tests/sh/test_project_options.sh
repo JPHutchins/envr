3d3fee2454ab2033e16d3327e2a19761f7bde390
J.P. Hutchins <jphutchins@gmail.com>
2023-04-01 22:44:38 -0700

    fix: change $ -> * since we are using eval now

--- tests/sh/test_project_options.sh
+++ tests/sh/test_project_options.sh
@@ -14,9 +14,9 @@ assertEqual "$OLD_ALS" "$(alias)"
 
 # cut off the leading characters
 if [[ -n "${BASH:-}" ]] ; then
-    assertEqual "$(echo $PS1 | cut -c 11-)" "(my long project name 1337 !_\$#? 3)"
+    assertEqual "$(echo $PS1 | cut -c 11-)" "(my long project name 1337 !_*#? 3)"
 elif [[ -n "${ZSH_VERSION:-}" ]] ; then
-    assertEqual "$(echo $PS1 | cut -c 8-)" "(my long project name 1337 !_\$#? 3) "
+    assertEqual "$(echo $PS1 | cut -c 8-)" "(my long project name 1337 !_*#? 3) "
 fi
 
 unsource
--- tests/windows/test_project_options.ps1
+++ tests/windows/test_project_options.ps1
@@ -8,7 +8,7 @@ function global:project_options () {
     . ./envr.ps1
 
     $PS1_OUT = prompt 6>&1
-    $TEST_RES += assertEqual "(my long project name 1337 !_$#? 3)  $OLD_PS1_OUT" $PS1_OUT
+    $TEST_RES += assertEqual "(my long project name 1337 !_*#? 3)  $OLD_PS1_OUT" $PS1_OUT
 
     unsource
 
